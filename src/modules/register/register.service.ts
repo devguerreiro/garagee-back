6c3da6fd7b3ce68a0eb0921c7aa61336898ccd2e
Luis <luiscvlh11@gmail.com>
2025-03-19 11:37:48 -0300

    fix: :bricks: altera versao do node para uma compativel com bcrypt

--- src/modules/register/register.service.ts
+++ src/modules/register/register.service.ts
@@ -5,14 +5,14 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './register.dto';
 import { RegisterRepository } from './register.repository';
 
-const saltOrRounds = 1;
+const saltOrRounds = 10;
 
 @Injectable()
 export class RegisterService {
   constructor(private readonly registerRepository: RegisterRepository) {}
 
   async createUser(data: CreateUserDTO) {
-    const hashedPassword = bcrypt.hashSync(data.password, saltOrRounds);
+    const hashedPassword = await bcrypt.hash(data.password, saltOrRounds);
     return await this.registerRepository.createUser({
       ...data,
       password: hashedPassword,
